933dadf9f7329630ee32be5ccd09c435f2765ae2
VladimirBelyaev <vogisa@mail.ru>
2024-07-25 15:32:09 +0300

    проработана передняя сторона карточки

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <Card>
     <template #front>
-      <h1>front</h1>
+      <h1 class="front__title">Поздравляем со свадьбой!</h1>
+      <p class="front__text">Желаем счастливой семейной жизни <br /> От всей души Вова и Кирилл</p>
     </template>
     <template #back>
       <h1>back</h1>
@@ -14,4 +15,19 @@ import Card from '@/components/Card.vue';
 </script>
 
 <style scoped lang="scss">
+.front__title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  white-space: nowrap;
+}
+
+.front__text {
+  position: absolute;
+  bottom: 3.2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: center;
+}
 </style>
